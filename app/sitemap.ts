83f8338f8f82d1ff001df78ce3fc5bd2274ed62b
roeebibas <roeebibas@gmail.com>
2026-09-02 14:03:13 +0300

    Add portfolio XML sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import type { MetadataRoute } from "next";
+
+import { projects } from "@/lib/portfolio-projects";
+
+const siteUrl = "https://roeebibas.github.io";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const lastModified = new Date();
+
+  const projectPages: MetadataRoute.Sitemap = projects.map((project) => ({
+    url: `${siteUrl}/work/${project.slug}/`,
+    lastModified,
+    changeFrequency: "monthly",
+    priority: 0.8,
+  }));
+
+  const piecePages: MetadataRoute.Sitemap = projects.flatMap((project) =>
+    project.images.map((image) => ({
+      url: `${siteUrl}/work/${project.slug}/${image.slug}/`,
+      lastModified,
+      changeFrequency: "yearly",
+      priority: 0.6,
+    })),
+  );
+
+  return [
+    {
+      url: `${siteUrl}/`,
+      lastModified,
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    ...projectPages,
+    ...piecePages,
+  ];
+}
